9b2b4f8035ea98f5fd4ef24476d4837c2c921964
Kakinohana Hideto <hidediving@gmail.com>
2022-07-27 11:07:46 +0900

    はじめ

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import stats
+
+plt.style.use('grayscale')
+plt.style.use('seaborn-whitegrid')
+np.random.seed(0)
+
